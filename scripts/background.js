f376cd307495b8dbf879811bdeac0523d339642d
Andy Nutt <andyrobertnutt@gmail.com>
2022-03-22 11:51:23 +0000

    Provide manifest V2 support for Firefox

--- scripts/background.js
+++ scripts/background.js
@@ -1,14 +1,14 @@
 // Update the icon or badge when content scripts require.
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.action === "update_icon") {
-    chrome.action.setIcon({
+    chrome.browserAction.setIcon({
       path: message.iconPath,
       tabId: sender.tab.id,
     });
   }
 
   if (message.action === "update_badge") {
-    chrome.action.setBadgeText({
+    chrome.browserAction.setBadgeText({
       text: message.badgeText > 0 ? message.badgeText : "",
       tabId: sender.tab.id,
     });
